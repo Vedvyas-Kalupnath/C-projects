eda11a35ef10962ead43d300914b202965f07e04
Vedvyas Kalupnath <vkalupnath@gmail.com>
2019-12-01 17:05:08 +0400

    Test program

---
+++ TestProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a test.");
+            Console.WriteLine("First program in c#");
+
+            Console.ReadLine();
+        }
+    }
+}
